0e0555e7b0551efffb477b465be11cecf110b53d
HERRR <osmithelias@outlook.com>
2025-07-31 16:26:10 -0400

    Created typewriter effect with styling and js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Hamburger Menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -12,3 +13,23 @@ document.querySelectorAll('.nav-item').forEach(n => n.addEventListener('click', 
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }))
+
+//Typewriter Effect
+const typeWriterText = document.querySelector('#hero-header');
+const text = "Hi, I'm Oneilia";
+
+function typeWriter(element, text, i = 0) {
+    if (i === 0) {
+        element.textContent = ""; // Clear the text content before starting
+    }
+    
+    element.textContent += text[i];
+    
+    if (i === text.length - 1) {
+    return;
+}
+  setTimeout(() => typeWriter(element, text, i + 1), 100);
+  
+}
+
+typeWriter(typeWriterText, text); 
